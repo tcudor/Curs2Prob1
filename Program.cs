a3fd2e2304b5d24751082155fa3c7d38d5da6a36
tudorg <tudorg@indsoft.ro>
2023-10-02 15:13:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace Curs2Prob1
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            
+            Console.Write("Introduceti numele persoanei: ");
+            string? nume = Console.ReadLine();
+
+            
+            Console.Write("Introduceti varsta persoanei: ");
+            bool canConvert = false;
+            while (!canConvert)
+            {
+                canConvert = Int32.TryParse(Console.ReadLine(), out int varsta);
+                if (!canConvert)
+                {
+                    Console.WriteLine("Varsta nu este un numar valid. Incercati din nou");
+                }
+                else
+                {
+                    if (varsta >= 18)
+                    {
+                        Console.WriteLine($"{nume} are drept de vot.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{nume} nu are drept de vot.");
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
